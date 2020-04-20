a6b7534c20b305a6f401fd6b48d5f9745496433d
Bryan McCloskey <bmccloskey@usgs.gov>
2020-04-20 12:10:37 -0400

    added contacts.php, datause-citation.php, newsletter.php, personnel.php, and publications.php

---
+++ personnel.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>EDEN Personnel - Everglades Depth Estimation Network (EDEN)</title>
+  <link rel="stylesheet" href="/eden/css/eden-dbweb-html5.css">
+  <script src="https://www.usgs.gov/scripts/analytics/usgs-analytics.js"></script>
+  <style>
+    table { border-collapse: collapse }
+    table, td, th { border: 1px solid #477489 }
+    td, th { padding: 2px }
+  </style>
+</head>
+<body>
+<?php require ($_SERVER['DOCUMENT_ROOT'] . '/eden/ssi/eden-head.txt'); ?>
+<?php require ($_SERVER['DOCUMENT_ROOT'] . '/eden/ssi/nav.php'); ?>
+<div style="overflow:hidden;padding-right:8px;background-color:white;min-height:825px;text-align:center"><!--Begin body of page -->
+  <p><strong>EDEN Project Team Leader:</strong></p>
+  <p><a href="https://www.usgs.gov/staff-profiles/eric-d-swain?qt-staff_profile_science_products=0#qt-staff_profile_science_products">Eric D. Swain</a><br>
+  Research Hydrologist<br> 
+  U.S. Geological Survey<br>
+  Caribbean-Florida Water Science Center<br>
+  NSU Center for Collaborative Research<br>
+  3321 College Avenue<br>
+  Davie, FL  33314<br>
+  email: <a href="mailto:edswain@usgs.gov">edswain@usgs.gov</a><br>
+  phone 954.377.5925<br>
+  fax 954.377.5901</p>
+  <p><strong>EDEN Project Team:</strong></p>
+  <p><a href="https://www.usgs.gov/staff-profiles/saira-haider?qt-staff_profile_science_products=0#qt-staff_profile_science_products">Saira Haider</a>, (<abbr title="U.S. Geological Survey">USGS</abbr>, Davie, <abbr title="Florida">FL</abbr>)<br>
+  <a href="https://www.usgs.gov/staff-profiles/heather-henkel?qt-staff_profile_science_products=3#qt-staff_profile_science_products">Heather Henkel</a>, (<abbr title="U.S. Geological Survey">USGS</abbr>, <abbr title="Saint">St.</abbr> Petersburg, <abbr title="Florida">FL</abbr>)<br>
+  <a href="https://www.usgs.gov/staff-profiles/bryan-mccloskey">Bryan McCloskey</a>, (<abbr title="U.S. Geological Survey">USGS</abbr>, <abbr title="Saint">St.</abbr> Petersburg, <abbr title="Florida">FL</abbr>)<br>
+  <a href='https://www.usgs.gov/staff-profiles/matthew-d-petkewich?qt-staff_profile_science_products=0#qt-staff_profile_science_products'>Matt Petkewich</a>, (<abbr title="U.S. Geological Survey">USGS</abbr>, Columbia, <abbr title="South Carolina">SC</abbr>)<br>
+  Pamela A. Telis (retired)<br>
+  Eduardo Patino (retired)<br>
+  Paul Conrads, (deceased)</p>
+</div><!--End body of page -->
+</div><!--End content and nav -->
+<div style="clear:both;width:100%;background-color:#4d7c86">
+  <span class="footer">Technical support for this Web site is provided by the <a href="http://www.usgs.gov/" class="footer">U.S. Geological Survey</a><br>This page is:
+<?php
+$filename = htmlentities($_SERVER['SCRIPT_NAME'], ENT_QUOTES); 
+echo "http://sofia.usgs.gov$filename";
+?>
+  <br>Comments and suggestions? Contact: <a href="https://archive.usgs.gov/archive/sites/sofia.usgs.gov/comments.html" class="footer">Heather Henkel - Webmaster</a><br>Last updated:
+<?php echo date ("F d, Y h:i A", getlastmod()); ?> (BJM)</span>
+</div>
+</body>
+</html>
