af23f1803078269f2bbf0008a39297e7648eca36
McCloskey, Bryan J <bmccloskey@usgs.gov>
2020-06-18 10:46:24 -0400

    database name change

--- ssi/login.php
+++ ssi/login.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect('localhost', 'edenweb', 'edenweb', 'eden_new');
+$db = mysqli_connect('localhost', 'edenweb', 'edenweb', 'eden');
 if (!$db)
 	exit('Could not connect to the database. Please click refresh on your browser or try again later.');
 ?>
